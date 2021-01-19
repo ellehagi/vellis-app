67d1fc5a25921fa87b2a051c4cb29ca05344383e
Elham Hagi <elhamhagi@outlook.com>
2021-01-19 17:32:25 +0000

    i created a logout component and a register screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import ProductScreen from './Screens/ProductScreen'
 import Header from './components/Header'
 import CartScreen from './Screens/CartScreen'
 import LoginScreen from './Screens/LoginScreen'
-
+import RegisterScreen from './Screens/RegisterScreen'
 
 
 
@@ -20,6 +20,7 @@ const App = () => {
       <Route path='/MenuScreen' component={MenuScreen} />
       <Route path='/product/:id' component={ProductScreen} />
       <Route path='/cart/:id?' component={CartScreen} />
+      <Route path='/register' component={RegisterScreen} />
       <Route path='/login' component={LoginScreen} />
       </Container>
     </Router>
